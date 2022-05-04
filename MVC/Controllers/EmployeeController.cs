2021abeaf0d342f88b2d9e2d71e985dcc51af8bc
Vinicius Nascimento <viniciusnp.1998@gmail.com>
2022-05-04 11:37:20 -0300

    Add project files.

---
+++ MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<MvcEmployeeModel> employeeList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("employee").Result;
+            employeeList = response.Content.ReadAsAsync<IEnumerable<MvcEmployeeModel>>().Result;
+
+            return View(employeeList);
+        }
+
+        public ActionResult ManagementEmployee(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new MvcEmployeeModel());
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync($"Employee/{id}").Result;
+                return View(response.Content.ReadAsAsync<MvcEmployeeModel>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ManagementEmployee(MvcEmployeeModel employee)
+        {
+            if (employee.Id == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", employee).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync($"Employee/{employee.Id}", employee).Result;
+                TempData["SuccessMessage"] = "Update Successfully";
+            }
+
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync($"Employee/{id}").Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            
+            return RedirectToAction("index");
+
+        }
+    }
+}
---
+++ MVC/Models/MvcEmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class MvcEmployeeModel
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage="Campo obrigatório!")]
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<decimal> Salary { get; set; }
+    }
+}
